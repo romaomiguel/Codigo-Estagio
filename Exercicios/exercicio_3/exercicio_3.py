97a2f6dc3ba5d067201daea1bbd94c84832ca863
Miguel Romão de Souza Menegusse <153843652+romaomiguel@users.noreply.github.com>
2024-09-15 00:32:17 -0400

    Atualização

--- Exercicios/exercicio_3/exercicio_3.py
+++ Exercicios/exercicio_3/exercicio_3.py
@@ -1,29 +1,26 @@
 import json
+import os
 
 # Função para processar os dados de faturamento
 def processar_faturamento(caminho_arquivo):
-
-    # Abra/fecha e leia o arquivo JSON
     with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
         dados = json.load(arquivo)
-    
-    # Extrair valores de faturamento ignorando dias sem faturamento
-    faturamentos = [float(dia['valor']) for dia in dados if float(dia['valor']) > 0] #Se valor for menor que 0 ele vira um "float" e é adicionado em faturamentos
-    
-    # Se não houver faturamento, retornamos valores nulos e zero
+
+    faturamentos = [float(dia['valor']) for dia in dados if float(dia['valor']) > 0]
+
     if not faturamentos:
         return None, None, 0 
-    
-    #Divisão das variaveis menor, maior, media extraindo do arquivo dados.json
+
     menor_faturamento = min(faturamentos)
     maior_faturamento = max(faturamentos)
     media_faturamento = sum(faturamentos) / len(faturamentos)
-    
+
     dias_acima_da_media = len([f for f in faturamentos if f > media_faturamento])
-    
+
     return menor_faturamento, maior_faturamento, dias_acima_da_media
 
-caminho_arquivo = r'C:\Users\amand\Desktop\Trabalho\Teste-de-Codigo-Estagio\exercicio_3\dados.json'  # O caminho completo para o arquivo JSON
+# Construir o caminho do arquivo relativo ao diretório do script
+caminho_arquivo = os.path.join(os.path.dirname(__file__), 'dados.json')  # Assume que o arquivo JSON está no mesmo diretório que o script
 menor, maior, dias_acima = processar_faturamento(caminho_arquivo)
 
 if menor is not None and maior is not None:
--- README.md
+++ README.md
@@ -14,10 +14,13 @@ Este projeto é composto por cinco desafios que foram resolvidos como parte do t
   - `exercicio_4.py`: Solução para o Exercicio 4
   - `Exercicio_5.py`: Solução para o Exercicio 5
 
+## Pré-requisitos
+Antes de executar o código, certifique-se de ter o Python instalado. Você pode verificar a versão do Python com o comando: 
+`python --version`.
+
 ## Como Executar
 Para instalar as dependências, use o comando 
 `python exercicio_1.py`.
 
-## Pré-requisitos
-Antes de executar o código, certifique-se de ter o Python instalado. Você pode verificar a versão do Python com o comando: 
-`python --version`.
+### Dicas
+Coloque o arquivo 'dados.json' na mesma pasta que esta o arquivo 'exercicio_3.py' para não ocasionar em erros
